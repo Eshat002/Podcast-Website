1576c88f1d00aa4eec5802a6ed37f1f911e2dc2b
eshat002 <eshatjubayer22@outlook.com>
2024-05-20 19:50:17 +0600

    project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './Components/LandingPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LandingPage.css'
+import Navbar from './Navbar/Navbar'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Navbar />
+            LandingPage
+        </div>
+    )
+}
+
+export default LandingPage
