e47be3f4e118607102a92790c1ec7b9f5ae6ef20
eshat002 <eshatjubayer22@outlook.com>
2024-07-14 14:42:59 +0600

    added CTA section

--- frontend/src/Components/CTA/CTA.jsx
+++ frontend/src/Components/CTA/CTA.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./CTA.css";
 import AppImage1 from "../../Img/appImage1.png";
 import AppImage2 from "../../Img/appImage2.png";
+import Headline from "../Headline/Headline";
+import SubHeadline from "../SubHeadline/SubHeadline";
+import PrimaryBtn from "../PrimaryBtn/PrimaryBtn";
+import { SiGooglepodcasts } from "react-icons/si";
+import { FaSpotify } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
 
 const CTA = () => {
   return (
@@ -11,7 +17,47 @@ const CTA = () => {
         <div className="col-lg-4 position-relative">
           <img className="cta-image-1" src={AppImage2} alt="download" />
         </div>
-        <div className="col-lg-4"></div>
+        <div className="col-lg-4 text-center">
+          <span
+            style={{
+              fontWeight: 700,
+              fontSize: "14px",
+              color: "#CD4631",
+              border: "1px solid #CD4631",
+              padding: "5px 10px",
+              borderRadius: "5px",
+              display: "inline-block",
+              marginTop: "110px",
+              marginBottom: "10px",
+            }}
+          >
+            BETA
+          </span>
+
+          <div className="headline-container px-3">
+            <Headline
+              Headline="Available now
+              Pod of Cast App"
+            />
+          </div>
+          <div
+            style={{ marginBottom: "60px" }}
+            className="sub-headline-container"
+          >
+            <SubHeadline SubHeadline="We just launched our podcast app!" />
+          </div>
+          <div className="button-container">
+            <PrimaryBtn btnText="DOWNLOAD NOW" />
+          </div>
+          <div className="icon-container-parent mt-4">
+            <p className="title">Content also available on:</p>
+            <div className="icon-container d-flex justify-content-center gap-3">
+              <SiGooglepodcasts fontSize={20} />
+              <FaSpotify fontSize={20} />
+              <FaYoutube fontSize={22} />
+            </div>
+          </div>
+        </div>
         <div className="col-lg-4 position-relative">
           <img className="cta-image-2" src={AppImage1} alt="download" />
         </div>
